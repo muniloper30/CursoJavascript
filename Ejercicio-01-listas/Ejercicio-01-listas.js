66e7da76f0515af4f8868cc1c6d5750c76cbb2da
muniloper30 <121830784+muniloper30@users.noreply.github.com>
2023-02-27 19:38:37 +0100

    Add files via upload

---
+++ Ejercicio-01-listas/Ejercicio-01-listas.js
@@ -0,0 +1,14 @@
+const lista = [
+    "Victor",
+    30,
+    true,
+    new Date(1992, 10, 2), 
+    {
+        titulo: "El hombre del viento",
+        autor: "Patrick Rothfuss",
+        fecha: new Date(2007, 2, 27),
+        url: "https://www.amazon.es/El-nombre-del-viento-EXITOS/dp/8401337208/ref=asc_df_8401337208/?tag=googshopes-21&linkCode=df0&hvadid=195309695167&hvpos=&hvnetw=g&hvrand=7105271605184760253&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1005419&hvtargid=pla-140716428315&psc=1"
+    },
+];
+
+console.log (lista)
